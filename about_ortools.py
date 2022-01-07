ae51d5debbf285b77048ff9d4b6c8cc99117de41
saddalmatian <saddalmatian@gmail.com>
2022-01-07 23:41:11 +0700

    initial ortools

---
+++ about_ortools.py
@@ -0,0 +1,22 @@
+from ortools.linear_solver import pywraplp
+from ortools.init import pywrapinit
+
+solver = pywraplp.Solver.CreateSolver('GLOP')
+
+x = solver.NumVar(0, 1, 'x')
+y = solver.NumVar(0, 2, 'y')
+
+ct = solver.Constraint(0, 2, 'ct')
+ct.SetCoefficient(x, 1)
+ct.SetCoefficient(y, 1)
+
+objective = solver.Objective()
+objective.SetCoefficient(x, 3)
+objective.SetCoefficient(y, 1)
+objective.SetMaximization()
+
+solver.Solve()
+print('Solution:')
+print('Objective value =', objective.Value())
+print('x =', x.solution_value())
+print('y =', y.solution_value())
